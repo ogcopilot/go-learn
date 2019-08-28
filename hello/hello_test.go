46e6f38d6d81221d431709a283b8f5f8dcbd02fe
Aaron Wagner <awagner@sibcycline.com>
2019-08-28 15:16:05 -0400

    Refactor hello to pass name

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
