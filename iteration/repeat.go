ddf7b283d7b64bd721f9a5d12bef084c0ba8d2cf
Aaron Wagner <awagner@sibcycline.com>
2019-08-30 17:25:37 -0400

    Wow! I just went down a rabbit hole
    Thinking I'd be cool and setup a repeat negative times test
    was maybe not the easiest thing to do. It certainly
    caused me to rapidly learn a lot more than this lesson
    initially offered.  I like that the lesson has exercises left
    the reader to figure out.  I wouldn't have known about
    how go handles errors.  A totally different mindset -- for sure.

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,12 +1,15 @@
 package iteration
 
-const repeatCount = 5
+import "errors"
 
 // Repeat will concatenate a passed in string to itself {x} number of times
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) (string, error) {
+	if repeatCount < 0 {
+		return "", errors.New("you can't repeat a character negative times. Duh")
+	}
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
-	return repeated
+	return repeated, nil
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,50 @@
 package iteration
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Repeat 'x' {n} of times for n = 3", func(t *testing.T) {
+		repeated, _ := Repeat("x", 3)
+		expected := "xxx"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("Repeat 'x' {n} of times for n = 0", func(t *testing.T) {
+		repeated, _ := Repeat("x", 0)
+		expected := ""
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("Repeat 'y' {n} of times for n = 1", func(t *testing.T) {
+		repeated, _ := Repeat("y", 1)
+		expected := "y"
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("Repeat 'x' {n} of times for n = -1", func(t *testing.T) {
+		_, err := Repeat("x", -1)
+		expect := true
+		got := err != nil
+		if got != expect {
+			t.Errorf("got %t wanted %t", got, expect)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", r.Intn(10))
 	}
 }
