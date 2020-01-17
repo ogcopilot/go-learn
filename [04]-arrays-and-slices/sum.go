62abac900a9c5d0ba2b17fdcb92305dcd48ae5e1
Aaron Wagner <awagner@sibcycline.com>
2020-01-17 18:27:42 -0500

    Cleaning up linting errors/warnings

--- [04]-arrays-and-slices/sum.go
+++ [04]-arrays-and-slices/sum.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func main() {
+	fmt.Println(Sum([]int{1, 2, 3, 4, 5, 6, 7, 8, 9}))
 }
--- [04]-arrays-and-slices/sum_test.go
+++ [04]-arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -13,4 +16,10 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func ExampleSum() {
+	summed := Sum([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	fmt.Println(summed)
+	// Output :45
 }
